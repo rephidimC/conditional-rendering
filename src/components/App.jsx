4d4dc35852a2bf2dc6be491b20c7ca5824ad1a49
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-15 13:00:11 +0100

    finished ternary operator and other modifications to the operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,22 +6,17 @@ import Login from "./Login";
 
 var isLoggedIn = false;
 
-function renderConditonally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+// function renderConditonally() {
+//   if (isLoggedIn === true) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
 
-//Is it possible to carry out the render functionality inline?
-//Yes, but there is a problem.
-//The If statement is in curly BroadcastChannel, which would make react see it as a statement, instead of an expression.
-//We can use the Ternary Operator
-
-function App() {
-  return <div className="container">{renderConditonally()}</div>;
-}
+// function App() {
+//   return <div className="container">{renderConditonally()}</div>;
+// }
 
 //But in React, how do we do it?
 
@@ -33,5 +28,26 @@ function App() {
 //     </div>
 //   );
 // }
+
+//Is it possible to carry out the render functionality inline?
+//Yes, but there is a problem.
+//The If statement is in curly BroadcastChannel, which would make react see it as a statement, instead of an expression.
+//We can use the Ternary Operator
+
+function App() {
+  return (
+    <div className="container">
+      {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+      {/* because isLoggedIn is a boolean already declared above, then we can further simplify. */}
+      {/* {isLoggedIn ? <h1>Hello</h1> : <Login />} */}
+      {/* let's assume we don't wanna do anything if condition is false, how do we proceed? */}
+      {/* {isLoggedIn ? <h1>Hello</h1> : null} */}
+      {/* there is another way to get this done. which is to use the &&...how this works is that it checks if the left condition is met, and if yes, it eecutes the right.
+      but if not, it simply does nothing.
+      e.g. {isLoggedIn ===true && <h1>Hello</h1>}
+      */}
+    </div>
+  );
+}
 
 export default App;
