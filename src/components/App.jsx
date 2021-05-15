bebdacacae073acfddf33b3e0988c01e50050e63
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-15 12:34:58 +0100

    prepared to introduce ternary operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,43 +4,34 @@ import Login from "./Login";
 //the goal is to dynamically render different things when certain conditions are met.
 //The below is how it is done using traditional if-else statements.
 
-// var isLoggedIn = false;
+var isLoggedIn = false;
 
-// function renderConditonally() {
-//   if (isLoggedIn === true) {
-//     return (<h1>Hello</h1>);
-//   } else {
-//     return (<form className="form">
-//       <input type="text" placeholder="Username" />
-//       <input type="password" placeholder="Password" />
-//       <button type="submit">Login</button>
-//     </form>);
-//   }
-// }
+function renderConditonally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
+}
+
+//Is it possible to carry out the render functionality inline?
+//Yes, but there is a problem.
+//The If statement is in curly BroadcastChannel, which would make react see it as a statement, instead of an expression.
+//We can use the Ternary Operator
+
+function App() {
+  return <div className="container">{renderConditonally()}</div>;
+}
+
+//But in React, how do we do it?
 
 // function App() {
 //   return (
 //     <div className="container">
-//       {/* <h1>Hello</h1>
-//       <form className="form">
-//         <input type="text" placeholder="Username" />
-//         <input type="password" placeholder="Password" />
-//         <button type="submit">Login</button>
-//       </form> */}
-//       {renderConditonally()}
+//       <h1>Hello</h1>
+//       <Login />
 //     </div>
 //   );
 // }
-
-//But in React, how do we do it?
-
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <Login />
-    </div>
-  );
-}
 
 export default App;
